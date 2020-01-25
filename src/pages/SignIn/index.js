2e830039d8007c45d518bb4a6e4b2e044b9cfe29
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-25 15:38:08 -0300

    feat: user login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+import { signInRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  const schema = Yup.object().shape({
-    email: Yup.string()
-      .email('Insira um e-mail válido')
-      .required('O e-mail é obrigatório'),
-    password: Yup.string().required('A senha é obrigatória'),
-  });
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
