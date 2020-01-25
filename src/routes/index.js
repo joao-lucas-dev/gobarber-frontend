02c73f76e9c7106b897b2ef049bba55f8fc9ef11
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-24 21:49:08 -0300

    feat: private routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Route from './Route';
 
 export default function Routes() {
   return (
@@ -12,8 +13,10 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+
+      <Route path="/" component={() => <h1>ERROR 404</h1>} />
     </Switch>
   );
 }
